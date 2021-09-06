cdc31067c1ce929549f1c6ace5a735ff92c27ec0
alejandro <amoro@alumni.unav.es>
2021-09-06 21:12:56 +0100

    Finished user listings

--- frontend/src/sections/User/components/UserListings.tsx
+++ frontend/src/sections/User/components/UserListings.tsx
@@ -1,27 +1,47 @@
 import { List, Typography } from "antd";
 import React from "react";
 import ListingCard from "../../../components/ListingCard";
+import { User_user_listings as IUserListings } from "../../../authUrl/../graphql/queries/__generated__/User";
 
-interface IProps {}
+interface IProps {
+  page: number;
+  userListings: IUserListings;
+  limit: number;
+  setPage: (pageNumber: number) => void;
+}
 
-const UserListings = () => {
-  // const userListingsList = (
-  //   <List
-  //     grid={{ gutter: 8, xs: 1, sm: 2, lg: 4 }}
-  //     dataSource={[]}
-  //     locale={{ emptyText: "User doesn't have any listings yet!" }}
-  //     pagination={{
-  //       position: "top",
-  //     }}
-  //     renderItem={(userListing) => (<ListingCard />)}
-  //   />
-  // );
+const UserListings = ({ page, limit, userListings, setPage }: IProps) => {
+  const userListingsList = (
+    <List
+      grid={{ gutter: 8, xs: 1, sm: 2, lg: 4 }}
+      dataSource={userListings.result}
+      locale={{ emptyText: "User doesn't have any listings yet!" }}
+      pagination={{
+        position: "top",
+        total: userListings.total,
+        current: page,
+        defaultPageSize: limit,
+        hideOnSinglePage: true,
+        showLessItems: true,
+        onChange: (page: number) => setPage(page),
+      }}
+      renderItem={(userListing) => (
+        <List.Item>
+          <ListingCard listing={userListing} />
+        </List.Item>
+      )}
+    />
+  );
   return (
     <div className="user-listings">
       <Typography.Title level={4} className="user-listings__title">
         Listings
       </Typography.Title>
-      <Typography.Paragraph></Typography.Paragraph>
+      <Typography.Paragraph>
+        This section contains the listings this user currently hosts and has
+        made available for bookings.
+      </Typography.Paragraph>
+      {userListingsList}
     </div>
   );
 };
