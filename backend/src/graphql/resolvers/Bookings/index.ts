bfafd1474b69b5a900e07d09c3b05d730f88f8c8
alejandro <amoro@alumni.unav.es>
2021-09-09 20:21:13 +0100

    Added resolver for Booking

---
+++ backend/src/graphql/resolvers/Bookings/index.ts
@@ -0,0 +1,19 @@
+import { IResolvers } from "apollo-server-express";
+import { Booking, Context, Listing } from "../../../lib/types";
+
+export const bookingResolvers: IResolvers = {
+  Booking: {
+    id: (booking: Booking): string => {
+      return booking._id.toString();
+    },
+    listing: async (
+      booking: Booking,
+      _,
+      { db }: Context
+    ): Promise<Listing | null> => {
+      return await db.listings.findOne({
+        _id: booking.listing,
+      });
+    },
+  },
+};
