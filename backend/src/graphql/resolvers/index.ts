7840f207b797c95621570d01e3b68da1ec493887
alejandro <amoro@alumni.unav.es>
2021-09-09 20:34:43 +0100

    Merging Booking and Listing resolvers with the other resolvers

--- backend/src/graphql/resolvers/index.ts
+++ backend/src/graphql/resolvers/index.ts
@@ -1,5 +1,12 @@
 import merge from "lodash.merge";
 import { viewerResolvers } from "./Viewers";
 import { userResolvers } from "./Users";
+import { listingResolvers } from "./Listings";
+import { bookingResolvers } from "./Bookings";
 
-export const resolvers = merge(viewerResolvers, userResolvers);
+export const resolvers = merge(
+  viewerResolvers,
+  userResolvers,
+  listingResolvers,
+  bookingResolvers
+);
