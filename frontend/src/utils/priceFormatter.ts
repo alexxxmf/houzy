f08a588d748b7596452ed5fa73de2713a83c9cdf
alejandro <amoro@alumni.unav.es>
2021-09-10 19:33:14 +0100

    Changed format

--- frontend/src/utils/priceFormatter.ts
+++ frontend/src/utils/priceFormatter.ts
@@ -1,5 +1,7 @@
 const priceFormatter = (price: number, round: boolean = true) => {
-  return round ? Math.round(price / 100) : price / 100;
+  // TODO: add currency handling
+  const formattedPrice = round ? Math.round(price / 100) : price / 100;
+  return `${formattedPrice}€`;
 };
 
 export default priceFormatter;
