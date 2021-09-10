a7f263330e438f4389c36bbf2c274cb1eba61508
alejandro <amoro@alumni.unav.es>
2021-09-10 19:38:24 +0100

    Wrapped listing card with a link to listing detial page

--- frontend/src/components/ListingCard/index.tsx
+++ frontend/src/components/ListingCard/index.tsx
@@ -1,6 +1,8 @@
 import Icon from "@ant-design/icons";
 import { Card, Typography } from "antd";
 import React from "react";
+import { Link } from "react-router-dom";
+import { priceFormatter } from "../../utils";
 
 interface IProps {
   listing: {
@@ -16,37 +18,39 @@ interface IProps {
 const { Text, Title } = Typography;
 
 const ListingCard = ({ listing }: IProps) => {
-  const { title, image, address, price, numOfGuests } = listing;
-  console.log("check|image", image);
+  const { title, image, address, price, numOfGuests, id } = listing;
+
   return (
-    <Card
-      hoverable
-      cover={
-        <div
-          className="listing-card__cover-img"
-          style={{ backgroundImage: `url(${image})` }}
-        />
-      }
-    >
-      <div className="listing-card__details">
-        <div className="listing-card__description">
-          <Title level={4} className="listing-card__price">
-            {price}
-            <span>/day</span>
-          </Title>
-          <Text strong ellipsis className="listing-card__title">
-            {title}
-          </Text>
-          <Text strong ellipsis className="listing-card__address">
-            {address}
-          </Text>
+    <Link to={`/listing/${id}`}>
+      <Card
+        hoverable
+        cover={
+          <div
+            className="listing-card__cover-img"
+            style={{ backgroundImage: `url(${image})` }}
+          />
+        }
+      >
+        <div className="listing-card__details">
+          <div className="listing-card__description">
+            <Title level={4} className="listing-card__price">
+              {priceFormatter(price)}
+              <span>/day</span>
+            </Title>
+            <Text strong ellipsis className="listing-card__title">
+              {title}
+            </Text>
+            <Text strong ellipsis className="listing-card__address">
+              {address}
+            </Text>
+          </div>
+          <div className="listing-card__dimensions listing-card__dimensions--guests">
+            <Icon type="user" />
+            <Text>{numOfGuests} guests</Text>
+          </div>
         </div>
-        <div className="listing-card__dimensions listing-card__dimensions--guests">
-          <Icon type="user" />
-          <Text>{numOfGuests} guests</Text>
-        </div>
-      </div>
-    </Card>
+      </Card>
+    </Link>
   );
 };
 
--- frontend/src/utils/priceFormatter.ts
+++ frontend/src/utils/priceFormatter.ts
@@ -1,7 +1,5 @@
-const priceFormatter = (price: number, round: boolean = true) => {
+export const priceFormatter = (price: number, round: boolean = true) => {
   // TODO: add currency handling
   const formattedPrice = round ? Math.round(price / 100) : price / 100;
   return `${formattedPrice}€`;
 };
-
-export default priceFormatter;
