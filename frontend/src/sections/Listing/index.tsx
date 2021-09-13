0e759859050ba90c5d800811c67e861db097657d
alejandro <amoro@alumni.unav.es>
2021-09-13 20:15:31 +0100

    Added some error and loading handling

--- frontend/src/sections/Listing/index.tsx
+++ frontend/src/sections/Listing/index.tsx
@@ -7,12 +7,17 @@ import {
 } from "../../graphql/queries/__generated__/Listing";
 import { RouteComponentProps } from "react-router-dom";
 import { Viewer } from "../../types";
+import { Layout } from "antd";
+import { PageSkeleton } from "../../components/PageSkeleton";
+import { ErrorBanner } from "../../components";
 
 const PAGE_LIMIT = 4;
 
 type IProps = RouteComponentProps<{ id: string }> & {
   viewer: Viewer;
 };
+
+const { Content } = Layout;
 
 export const Listing = ({ match, viewer }: IProps) => {
   const userId = match.params.id;
@@ -25,5 +30,23 @@ export const Listing = ({ match, viewer }: IProps) => {
   } = useQuery<IListingData, IListingVariables>(QUERY_LISTING, {
     variables: { id: userId, page: bookingsPage, limit: PAGE_LIMIT },
   });
+
+  if (listingError) {
+    return (
+      <Content className="listing">
+        <ErrorBanner description="This listing may not exist or we've encountered an error. Please try again soon." />
+        <PageSkeleton />
+      </Content>
+    );
+  }
+
+  if (listingLoading) {
+    return (
+      <Content className="listing">
+        <PageSkeleton />
+      </Content>
+    );
+  }
+
   return <h2>Listing</h2>;
 };
