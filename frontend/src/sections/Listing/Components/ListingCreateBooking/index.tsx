102bb63b57d5056c10279dad37f47acc7ed6ad93
alejandro <amoro@alumni.unav.es>
2021-09-16 20:52:12 +0100

    Adding formatted price and started to create the shape of the whole component

--- frontend/src/sections/Listing/Components/ListingCreateBooking/index.tsx
+++ frontend/src/sections/Listing/Components/ListingCreateBooking/index.tsx
@@ -1,7 +1,8 @@
-import { Card } from "antd";
+import { Card, Typography } from "antd";
 import React from "react";
 import dayjs, { Dayjs } from "dayjs";
 import { DatePicker } from "../../../../components";
+import { priceFormatter } from "../../../../utils";
 
 interface IProps {
   price: number;
@@ -15,10 +16,21 @@ const ListingCreateBooking = ({
   price,
   checkInDate,
   setCheckInDate,
+  checkOutDate,
+  setCheckOutDate,
 }: IProps) => {
   return (
     <div className="listing-booking">
-      <Card className="listing-booking__card">{price}</Card>
+      <Card className="listing-booking__card">
+        <div>
+          <Typography.Paragraph>
+            <Typography.Title level={2} className="listing-booking__card-title">
+              {priceFormatter(price)}
+              <span>/day</span>
+            </Typography.Title>
+          </Typography.Paragraph>
+        </div>
+      </Card>
       <DatePicker onChange={setCheckInDate} value={checkInDate} />
     </div>
   );
