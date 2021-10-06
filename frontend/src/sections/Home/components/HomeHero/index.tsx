80111160e583808fd4786d0893625794b14f7912
alejandro <amoro@alumni.unav.es>
2021-10-06 10:45:28 +0100

    Added more stuff to home page

--- frontend/src/sections/Home/components/HomeHero/index.tsx
+++ frontend/src/sections/Home/components/HomeHero/index.tsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { Card, Col, Row, Typography, Input } from "antd";
 
 import torontoImg from "../../assets/toronto.jpg";
-import sfImg from "../../assets/san-francisco.jpg";
 import laImg from "../../assets/los-angeles.jpg";
 import londonImg from "../../assets/london.jpg";
 import dubaiImg from "../../assets/dubai.jpg";
-import cancunImg from "../../assets/cancun.jpg";
 
 const { Title } = Typography;
 const { Search } = Input;
 
-export const HomeHero = () => {
+interface IProps {
+  onSearch: (value: string) => void;
+}
+
+export const HomeHero = ({ onSearch }: IProps) => {
   return (
     <div className="home-hero">
       <div className="home-hero__search">
@@ -24,8 +27,32 @@ export const HomeHero = () => {
           size="large"
           enterButton
           className="home-hero__search-input"
+          onSearch={onSearch}
         />
       </div>
+
+      <Row gutter={12} className="home-hero__cards">
+        <Col xs={12} md={6}>
+          <Link to="/listings/toronto">
+            <Card cover={<img src={torontoImg} />}>Toronto</Card>
+          </Link>
+        </Col>
+        <Col xs={12} md={6}>
+          <Link to="/listings/london">
+            <Card cover={<img src={londonImg} />}>London</Card>
+          </Link>
+        </Col>
+        <Col xs={12} md={6}>
+          <Link to="/listings/dubai">
+            <Card cover={<img src={dubaiImg} />}>Dubai</Card>
+          </Link>
+        </Col>
+        <Col xs={12} md={6}>
+          <Link to="/listings/los%20angeles">
+            <Card cover={<img src={laImg} />}>Los Angeles</Card>
+          </Link>
+        </Col>
+      </Row>
     </div>
   );
 };
