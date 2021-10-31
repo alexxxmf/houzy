0aec0a0fedfaf690840e81ccf4fd64ca2b599132
alejandro <amoro@alumni.unav.es>
2021-10-31 10:12:38 +0100

    Added some stuff for the NotFound page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@
 require("dotenv").config();
 
 import express, { Application } from "express";
+import compression from "compression";
 import cookieParser from "cookie-parser";
 import { ApolloServer } from "apollo-server-express";
 import { connectDatabase } from "./database";
@@ -12,6 +13,10 @@ const mount = async (app: Application) => {
 
   app.use(express.json({ limit: "5mb" }));
   app.use(cookieParser(process.env.SECRET));
+  app.use(compression());
+
+  app.use(express.static(`${__dirname}/client`));
+  app.get("*", (_req, res) => res.sendFile(`${__dirname}/client/index.html`));
 
   const server = new ApolloServer({
     typeDefs,
--- frontend/src/sections/NotFound/index.tsx
+++ frontend/src/sections/NotFound/index.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Typography, Empty, Layout } from "antd";
+import { Link } from "react-router-dom";
+
+const { Text } = Typography;
+const { Content } = Layout;
 
 export const NotFound = () => {
-  return <h2>Not Found</h2>;
+  return (
+    <Content className="not-found">
+      <Empty
+        description={
+          <Fragment>
+            <Text className="not-found__description-title">
+              Uh oh! Something went wrong :()
+            </Text>
+            <Text className="not-found__description-subtitle">
+              The page you're looking for can't be found
+            </Text>
+          </Fragment>
+        }
+      />
+
+      <Link
+        to="/"
+        className="not-found__cta ant-btn ant-btn-primary ant-btn-lg"
+      >
+        Go to Home
+      </Link>
+    </Content>
+  );
 };
